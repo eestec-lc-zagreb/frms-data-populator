c00908e1cabd8b9b42b706bededbf7c347d1f280
Alen Hrga <ahrga93@gmail.com>
2017-05-06 17:13:16 +0200

    Added Spring Configuration to this app

--- src/main/java/hr/eestec_zg/populator/Populator.java
+++ src/main/java/hr/eestec_zg/populator/Populator.java
@@ -1,9 +1,25 @@
 package hr.eestec_zg.populator;
 
+import hr.eestec_zg.populator.config.AppConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Populator {
+    private static final Logger logger = LoggerFactory.getLogger(Populator.class);
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        logger.info("Starting...");
+
+        if (System.getProperty("spring.profiles.active") == null) {
+            System.setProperty("spring.profiles.active", "production");
+        }
+
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        ctx.close();
+
+        logger.info("Finishing...");
     }
 
 }
